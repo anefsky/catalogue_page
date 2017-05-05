d20cb22f78660250d33b5bbe4d46d5bd16bb667c
Andrew Nefsky <anefsky@gmail.com>
2017-05-05 15:52:18 -0400

    Changed base dir

--- public/js/components/item-detail.component.js
+++ public/js/components/item-detail.component.js
@@ -1,8 +1,8 @@
 angular.module('myApp').component('itemDetail', {
-	templateUrl: '/js/components/item-detail.template.html',
+	templateUrl: 'js/components/item-detail.template.html',
 	controller: function ($http, $routeParams) {
 
-		$http.get('/data/' + 'sale-items.json')
+		$http.get('data/' + 'sale-items.json')
 			.then( response => {
 				var itemChoices = response.data;
 				var itemId = $routeParams.itemId;
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,16 +7,13 @@ app.config(
 	function($routeProvider, $locationProvider){
 		$routeProvider.
 		when('/', {
-			template: '<items-list></items-list>'
- 		}).
+			template: `<items-list></items-list>`
+		}).
 		when('/item/:itemId', {
-			template: '<item-detail></item-detail>'
+			template: `<item-detail></item-detail>`
 		}).
 		otherwise({
 			redirectTo: '/404'
 		});
-		$locationProvider.html5Mode({
-			enabled: true,
-			requireBase: false
-		});
+		$locationProvider.html5Mode(true);
 })
